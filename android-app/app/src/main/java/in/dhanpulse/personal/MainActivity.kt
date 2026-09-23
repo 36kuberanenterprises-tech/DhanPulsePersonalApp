fb359e8649a1082c0751f848b21967d3617d45f3
36kuberanenterprises-tech <36kuberanenterprises@gmail.com>
2026-09-23 23:16:42 +0530

    Fix diagnostic slice model import

--- android-app/app/src/main/java/in/dhanpulse/personal/MainActivity.kt
+++ android-app/app/src/main/java/in/dhanpulse/personal/MainActivity.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import `in`.dhanpulse.personal.model.AnalysisResponse
 import `in`.dhanpulse.personal.model.AccountSummary
+import `in`.dhanpulse.personal.model.BacktestSlice
 import `in`.dhanpulse.personal.ui.DhanPulseViewModel
 import kotlin.math.abs
 
