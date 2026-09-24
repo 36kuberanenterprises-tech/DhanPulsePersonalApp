8cb90f9536f12373641d6350e77e96802d1389e1
36kuberanenterprises-tech <36kuberanenterprises@gmail.com>
2026-09-24 15:45:49 +0530

    Fix Call Book TextAlign import

--- android-app/app/src/main/java/in/dhanpulse/personal/MainActivity.kt
+++ android-app/app/src/main/java/in/dhanpulse/personal/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
